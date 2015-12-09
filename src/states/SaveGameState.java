fbe011009cf0f9f21a634fcaa661c5f1de13774a
Stormi Sprague <StormiSprague@gmail.com>
2015-12-08 16:56:00 -0800

    Implemented load/save

--- src/states/SaveGameState.java
+++ src/states/SaveGameState.java
@@ -1,93 +1,109 @@
-/**
- * SaveGameState.java
- * Author: Corbin Staaben
- * Description: SaveGameState is a piece of the State pattern implemented to manage game flow. It allows the user to
- * save the game at the current point in the game using the Java API's Serializable interface.
- */
 package states;
 
 import java.io.*;
-import java.util.Calendar;
+import java.util.*;
 
-public class SaveGameState implements TriviaMazeState, Serializable {
+public class SaveGameState implements TriviaMazeState, Serializable{
 
 	private TriviaMaze triviaMaze;
-	private transient FileOutputStream fos;
-	private transient ObjectOutputStream oos;
-
-	private static final String SAVEPATH = "./src/savedGames/"; // contains the path to the directory containing saved games
-
-	/**
-	 * SaveGameState saves the reference that manages game flow and all pertinent information for this "state" to function
-	 * @param triviaMaze - the TriviaMaze object that contains, or holds references to, all the information necessary
-	 *                   for the game, and this state, to function.
-	 */
-
+    private static final String SAVE_PATH = "./src/savedGames/";
+	
 	public SaveGameState(TriviaMaze triviaMaze) {
 		this.triviaMaze = triviaMaze;
 	}
-
-	/**
-	 * (description of saveGame) It will then automatically transition the user into the main menu state of the program.
-	 */
+	
 	@Override
 	public void saveGame() {
-		String filename = buildTimestamp();
+	 //Save each object that changes state to a data file that can be read in when the game is loaded
+     //Enter a save name for the game, when we load we display all the save files located in the save folder
+      
+      String saveName = "";
+      
+      saveName = getSaveName();
+      
+      //send all required objects to serializable storage file
+      // Write to disk with FileOutputStream
+      try{
+    	  FileOutputStream f_out = new FileOutputStream(SAVE_PATH+saveName+".data");
 
-		try {
-			fos = new FileOutputStream(SAVEPATH + filename);
-			oos = new ObjectOutputStream(fos);
+    	  // Write object with ObjectOutputStream
+    	  ObjectOutputStream obj_out = new ObjectOutputStream (f_out);
 
-			oos.writeObject(triviaMaze);
-		}
-		catch(FileNotFoundException fnfe) {
-			fnfe.printStackTrace();
-		}
-		catch(IOException ioe) {
-			ioe.printStackTrace();
-		}
-
-		System.out.println("Saved Game!");
-		triviaMaze.setState(triviaMaze.getMainMenuState());
+    	  // Write object out to disk
+    	  obj_out.writeObject (triviaMaze);
+    	  //obj_out.close();
+      }
+      
+      catch(FileNotFoundException e){
+    	  System.out.println(e);
+      }
+      
+      catch(IOException ioe){
+    	  System.out.println(ioe);
+      }
+      
+      System.out.println("Your game has been saved as "+saveName);
+      
+      triviaMaze.setState(triviaMaze.getMainMenuState());
+	}
+	
+	private String getSaveName(){
+		String temp = "";
+		System.out.println("Name this game, and press enter");
+	      
+	      while (!triviaMaze.KB.hasNext("[A-Za-z]+")) {
+	        System.out.println("Please use only alphabetical characters to name your game");
+	        triviaMaze.KB.next();
+	      }
+	      temp = triviaMaze.KB.next();
+	      return temp;
 	}
 
-	/**
-	 * buildTimestamp() is a helper method to build a timestamp of the current time for use in naming saved game files
-	 * @return - the String representation of the current timestamp (month-day-year_hour:minute:second).
-	 */
-	private String buildTimestamp() {
-		String result = "";
+	@Override
+	public void newGame() {
+		// TODO Auto-generated method stub
 
-		result += Calendar.getInstance().get(Calendar.MONTH)+1;
-		result += "-" + Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
-		result += "-" + Calendar.getInstance().get(Calendar.YEAR);
-		result += "_" + Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-		result += "-" + Calendar.getInstance().get(Calendar.MINUTE);
-		result += "-" + Calendar.getInstance().get(Calendar.SECOND);
-
-		return result;
 	}
 
-	/**
-	 * All methods beyond this point are stubbed out because they are a part of the TriviaMazeState interface.
-	 * They are not necessary for this "state" to function and they are not, nor should they ever be, implemented here.
-	 */
+	@Override
+	public void loadGame() {
+		// TODO Auto-generated method stub
+
+	}
 
 	@Override
-	public void newGame() {}
+	public void adminDB() {
+		// TODO Auto-generated method stub
+
+	}
+
 	@Override
-	public void loadGame() {}
+	public void navigateMaze() {
+		// TODO Auto-generated method stub
+
+	}
+
 	@Override
-	public void adminDB() {}
+	public void answerQuestion(String direction) {
+		// TODO Auto-generated method stub
+
+	}
+
 	@Override
-	public void navigateMaze() {}
+	public void mainMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
-	public void answerQuestion(String direction) {}
+	public void exitMaze() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	@Override
-	public void mainMenu() {}
-	@Override
-	public void exitMaze() {}
-	@Override
-	public void endGame() {}
+	public void endGame() {
+		// TODO Auto-generated method stub
+	}
 
 }
